af506b18e3f3f87c547112c755339eb5455dd4eb
physics4life3290 <83435494+physics4life3290@users.noreply.github.com>
2025-07-11 22:56:57 -0400

    One and Two dimensional Cartesian and Spherical grids are now working. A "How To" has been attached in the grids/ directory.

--- src/ARC.jl
+++ src/ARC.jl
@@ -18,8 +18,46 @@ export Construct2DCartesian
 export Construct2DSpherical
 
 function run_simulation()
-    println("ARC simulation running...")
-    println("The sum of the first three primes is: $(2+3+5)")
+    
+    cart1Dgrid = Construct1DCartesian(1.0, 100, 3, 0.5, "cm")
+
+    t = 1.0
+    CFL = 0.3
+    # --- Initial condition ---
+    function initial_condition(x)
+        return exp.(-100 .* (x .- 0.5).^2)
+    end
+
+    function build1DLinearAdvection(cart1Dgrid, t, CFL)
+        u = initial_condition(cart1Dgrid.xcoord.centers)
+        u0 = copy(u)
+        N = length(u)
+        a = 1.0
+        dt = CFL * cart1Dgrid.xcoord.spacing / abs(a)
+        nt = Int(floor(t / dt))
+        
+        # --- Build BTCS matrix (periodic) ---
+        r = a * dt / (2 * cart1Dgrid.xcoord.spacing)
+        A = [i == j ? 1.0 : 0.0 for i in 1:N, j in 1:N]
+
+        for i in 1:N
+            A[i, mod1(i+1, N)] +=  r
+            A[i, mod1(i-1, N)] += -r
+        end
+    end
+    # --- Set up animation ---
+    anim = @animate for n in 1:nt
+        u = A \ u
+
+        if n % 1 == 0 || n == 1 || n == nt
+            plot(cart1Dgrid.xcoord.centers, u0, lw=2, label="Initial", xlabel="x", ylabel="u", legend=:topright)
+            plot!(cart1Dgrid.xcoord.centers, u, lw=2, label="t = $(round(n*dt, digits=2))")
+        end
+    end
+
+    # --- Save animation as GIF ---
+    gif(anim, "btcs_advection.gif", fps=100)
+
 end
 
 end # module
--- src/grids/_2Dimension/Construct2DCartesian.jl
+++ src/grids/_2Dimension/Construct2DCartesian.jl
@@ -10,18 +10,35 @@ struct CartesianGrid2D
     ybounds::Tuple{Float64, Float64}
     xghost_bounds::Tuple{Float64, Float64}
     yghost_bounds::Tuple{Float64, Float64}
+    grid_center::Float64
+    cell_areas::Matrix{Float64}
     units::String
 end
 
 
-function Construct2DCartesian(xdomain_length::Float64, ydomain_length::Float64, xzones::Int, yzones::Int, ghost_zones::Int, grid_center::Float64, units::String = "cm")
+function Construct2DCartesian(
+    xdomain_length::Float64,
+    ydomain_length::Float64,
+    xzones::Int,
+    yzones::Int,
+    ghost_zones::Int,
+    grid_center::Float64,
+    units::String
+)
     xcoord = ConstructUniformAxis(xdomain_length, xzones, ghost_zones, grid_center, :cartesian)
     ycoord = ConstructUniformAxis(ydomain_length, yzones, ghost_zones, grid_center, :cartesian)
+
     xbounds = (-xdomain_length/2 + grid_center, xdomain_length/2 + grid_center)
     ybounds = (-ydomain_length/2 + grid_center, ydomain_length/2 + grid_center)
+
+    cell_areas = xcoord.spacing * ycoord.spacing .* ones(xzones, yzones)
+
     xghost_bounds = (-xdomain_length/2 + grid_center - ghost_zones * xcoord.spacing, 
                      xdomain_length/2 + grid_center + ghost_zones * xcoord.spacing)
+
     yghost_bounds = (-ydomain_length/2 + grid_center - ghost_zones * ycoord.spacing, 
                      ydomain_length/2 + grid_center + ghost_zones * ycoord.spacing)
-    return CartesianGrid2D(xcoord, ycoord, xbounds, ybounds, xghost_bounds, yghost_bounds, units)
+
+    return CartesianGrid2D(xcoord, ycoord, xbounds, ybounds, xghost_bounds, yghost_bounds, grid_center, cell_areas, units) 
 end
+
